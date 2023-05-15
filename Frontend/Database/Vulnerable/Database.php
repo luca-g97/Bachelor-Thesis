f6be61dfc23cc0376919ff7d47d94af9a30975b4
luca-g97 <lg-gaming@web.de>
2023-05-15 23:11:14 +0200

    All websites are now working with hidden folders

--- Frontend/Database/Vulnerable/Database.php
+++ Frontend/Database/Vulnerable/Database.php
@@ -75,7 +75,7 @@ $databases = array(
 /* ---- Interface settings ---- */
 
 // Theme! If you want to change theme, save the CSS file in same folder of phpliteadmin or in folder "themes"
-$theme = './Database/phpliteadmin.css';
+$theme = '.Database/phpliteadmin.css';
 
 // the default language! If you want to change it, save the language file in same folder of phpliteadmin or in folder "languages"
 // More about localizations (downloads, how to translate etc.): https://bitbucket.org/phpliteadmin/public/wiki/Localization
